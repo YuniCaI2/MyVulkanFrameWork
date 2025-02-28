b6ca9f8a9726b2f604f906c280147e1fbd4e549b
YuniCaI2 <510927726@qq.com>
2025-02-28 08:48:38 +0800

    fix the format mistake

--- ProgramCode/Presentation/SwapChain.h
+++ ProgramCode/Presentation/SwapChain.h
@@ -33,7 +33,7 @@ namespace VK {
         static VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
         static VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
         static VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities, GLFWwindow* window);
-        void createImageViews(VkFormat swapchainFormat);
+        void createImageViews(VkFormat swapChainFormat);
     };
 }
 
